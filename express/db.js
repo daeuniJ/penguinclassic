96b2e1c06fa6a56ba09da009d4b3da2ded6168ca
daeuniJ <daeunjeong161@gmail.com>
2023-02-05 21:22:09 +0900

    0205 수정

--- express/db.js
+++ express/db.js
@@ -14,9 +14,50 @@ connection.connect(function(err) {
   console.log("Connected!");
 });
 
+//공지사항 추출할 때
+function getNoti(callback){
+  connection.query('SELECT * FROM notice ORDER BY id desc', (err, rows, fields) => {
+    if(err) throw err;
+    callback(rows);
+  })
+}
 
+//공지사항 입력할 때
+function insertNoti(writer,date,title,cont, callback){
+  connection.query(`INSERT INTO notice(writer,date,title,cont) VALUES('${writer}','${date}','${title}','${cont}')`, (err)=>{
+    if(err) throw err;
+    callback();
+  })
+}
 
-//getMemo 내보내기 (이 과정이 있어야 이 함수를 다른 곳에서도 연동해서 쓸 수 있다)
-// module.exports = {
-  
-// }
+//공지사항 상세페이지에 출력할 때
+function getNotiByid(id, callback){
+  connection.query(`SELECT * FROM notice WHERE id=${id}`, (err,row) => {
+    if(err) throw err;
+    callback(row);
+  })
+}
+
+//공지사항 삭제할 때
+function deleteNByid(id, callback){
+  connection.query(`DELETE FROM notice WHERE id=${id}`, (err) => {
+    if(err) throw err;
+    callback();
+  })
+}
+
+//공지사항 수정할 때
+function updateNoti(id,writer,date,title,cont, callback) {
+  connection.query(`UPDATE notice SET writer="${writer}",date="${date}",title="${title}",cont="${cont}" where id=${id}`, (err) => {
+    if(err) throw err;
+    callback();
+  })
+}
+
+module.exports = {
+  getNoti,
+  insertNoti,
+  getNotiByid,
+  deleteNByid,
+  updateNoti,
+}
--- express/routes/route.js
+++ express/routes/route.js
@@ -13,25 +13,72 @@ router.get('/', (req, res) => {
 
 //공지사항 페이지
 router.get('/noticeList', (req, res) => {
-  res.render('noticeList_page');
-  
+  db.getNoti((rows)=>{
+    res.render('noticeList_page',{rows:rows});
+  })
 });
 
 //공지사항 작성 페이지
 router.get('/noticeWrite', (req, res) => {
   res.render('noticeWrite_page');
-  
 });
+
+//작성페이지 액션
+router.post('/newWrite',(req,res)=>{
+  let param = JSON.parse(JSON.stringify(req.body));
+  let writer = param['writer'];
+  let date = param['date'];
+  let title = param['title'];
+  let cont = param['cont'];
+  db.insertNoti(writer,date,title,cont, ()=>{
+    res.redirect('/noticeList')
+  })
+})
 
 //공지사항 수정 페이지
 router.get('/noticeUpdate', (req, res) => {
   res.render('noticeUpdate_page');
 });
 
+//공지사항 수정 페이지에 데이터 가져오기
+router.get('/updates', (req, res)=>{
+  let id = req.query.id;
+
+  db.getNotiByid(id, (row) => {
+    res.render('noticeUpdate_page',{ row:row[0] })
+  })
+})
+
+//공지사항 수정 액션
+router.post('/updateNoti', (req, res) => {
+  let param = JSON.parse(JSON.stringify(req.body));
+  let id = param['id'];
+  let writer = param['writer'];
+  let date = param['date'];
+  let title = param['title'];
+  let cont = param['cont']; 
+  db.updateNoti(id,writer,date,title,cont, ()=>{
+    res.redirect('/noticeList')
+  })
+})
+
 //공지사항 상세 페이지
 router.get('/noticeCon', (req, res) => {
-  res.render('noticeCon_page');
+  let id = req.query.id;
+
+  db.getNotiByid(id, (row)=>{
+    res.render('noticeCon_page', { row:row[0] });
+  })
 });
+
+//공지사항 삭제
+router.get('/deletNoti',(req, res) => {
+  let id = req.query.id;
+
+  db.deleteNByid(id,() => {
+    res.redirect('/noticeList')
+  })
+})
 
 //로그인 페이지
 router.get('/logIn', (req, res) => {
