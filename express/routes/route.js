0f25145a395e04e345791719822d98e69e619d0b
daeuniJ <117437676+daeuniJ@users.noreply.github.com>
2025-09-11 14:43:25 +0900

    Update route.js

--- express/routes/route.js
+++ express/routes/route.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router =  express.Router();
 const fs = require('fs');
 const path = require('path');
-const db = require('./../db.js');
+//const db = require('./../db.js');
 
 
 //메인페이지
